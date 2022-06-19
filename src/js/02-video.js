20aab72bec095df1be77c6addd9d91ad7b3831b9
Raya Shcherban <90385588+Raisiya@users.noreply.github.com>
2022-06-19 14:06:32 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,7 +8,6 @@ const onPlay = throttle((data) =>
     localStorage.setItem(CURRENT_TIME, data.seconds), 1000);
 
 player.on('timeupdate', onPlay);
-
 const time = localStorage.getItem(CURRENT_TIME);
 if (time) {
     player.setCurrentTime(JSON.parse(time));  
